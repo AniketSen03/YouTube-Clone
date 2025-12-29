2155e56b668fbdd275ac3d6d7915b5c941f5a545
Aniket <aniketsen521@gmail.com>
2025-12-29 08:30:47 +0530

    channel logo and div change btn

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -8,12 +8,36 @@ import Skeleton from './Skeleton';
 
 const Feed = ({ category, converter, day }) => {
   const [videos, setvideos] = useState([])
-  const [imgdata, setimgdata] = useState(null)
+  const [imgdata, setimgdata] = useState({})
+
+  const fetchChannelLogos = async (items) => {
+    const ids = items.map(v => v.snippet.channelId).join(',');
+
+    const res = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/channels`,
+      {
+        params: {
+          part: "snippet",
+          id: ids,
+          key: API_KEY
+        }
+      }
+    );
+
+    const map = {};
+    res.data.items.forEach(ch => {
+      map[ch.id] = ch.snippet.thumbnails.default.url;
+    });
+
+    setimgdata(map);
+  };
+
 
   const fetchYouTubeData = async () => {
     try {
       const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&videoCategoryId=${category}&key=${API_KEY}`);
       setvideos(res.data.items);
+      fetchChannelLogos(res.data.items)
 
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -44,61 +68,69 @@ const Feed = ({ category, converter, day }) => {
   };
 
 
- return (
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-4">
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-4">
 
-    {/* Skeleton loading */}
-    {videos.length === 0 &&
-      [...Array(8)].map((_, i) => (
-        <div key={i}>
-          <Skeleton className="h-40 w-full rounded-xl" />
-          <Skeleton className="h-4 mt-2 w-3/4" />
-          <Skeleton className="h-3 mt-1 w-1/2" />
-        </div>
-      ))
-    }
-
-    {/* Videos */}
-    {videos.length > 0 &&
-      videos.map((video) => (
-        <Link
-          to={`video/${video.snippet.categoryId}/${video.id}`}
-          key={video.id}
-          className="text-zinc-200 hover:text-white"
-        >
-          {/* Thumbnail */}
-          <div className="relative">
-            <img
-              src={video?.snippet?.thumbnails?.medium?.url}
-              alt=""
-              className="w-full rounded-xl"
-            />
-            <span className="absolute bottom-2 right-2 bg-black/80 text-xs px-2 py-[2px] rounded">
-              {formatDuration(video.contentDetails.duration)}
-            </span>
+      {/* Skeleton loading */}
+      {videos.length === 0 &&
+        [...Array(8)].map((_, i) => (
+          <div key={i}>
+            <Skeleton className="h-40 w-full rounded-xl" />
+            <Skeleton className="h-4 mt-2 w-3/4" />
+            <Skeleton className="h-3 mt-1 w-1/2" />
           </div>
+        ))
+      }
 
-          {/* Info */}
-          <div className="mt-2">
-            <h3 className="font-semibold text-sm leading-snug line-clamp-2">
-              {video?.snippet?.title}
-            </h3>
+      {/* Videos */}
+      {videos.length > 0 &&
+        videos.map((video) => (
+          <Link
+            to={`video/${video.snippet.categoryId}/${video.id}`}
+            key={video.id}
+            className="text-zinc-200 hover:text-white"
+          >
+            {/* Thumbnail */}
+            <div className="relative">
+              <img
+                src={video?.snippet?.thumbnails?.medium?.url}
+                alt=""
+                className="w-full rounded-xl"
+              />
+              <span className="absolute bottom-2 right-2 bg-black/80 text-xs px-2 py-[2px] rounded">
+                {formatDuration(video.contentDetails.duration)}
+              </span>
+            </div>
 
-            <p className="text-zinc-400 text-sm mt-1">
-              {video?.snippet?.channelTitle}
-            </p>
+            <div className="flex gap-3 mt-2">
+              <img
+                src={imgdata[video.snippet.channelId]}
+                className="w-9 h-9 rounded-full"
+                alt=""
+              />
 
-            <p className="text-zinc-400 text-sm">
-              {converter(video?.statistics?.viewCount)} views •{" "}
-              {day(moment(video?.snippet?.publishedAt).fromNow())}
-            </p>
-          </div>
-        </Link>
-      ))
-    }
+              {/* Info */}
+              <div className="mt-2">
+                <h3 className="font-semibold text-sm leading-snug line-clamp-2">
+                  {video?.snippet?.title}
+                </h3>
 
-  </div>
-);
+                <p className="text-zinc-400 text-sm mt-1">
+                  {video?.snippet?.channelTitle}
+                </p>
+
+                <p className="text-zinc-400 text-sm">
+                  {converter(video?.statistics?.viewCount)} views •{" "}
+                  {day(moment(video?.snippet?.publishedAt).fromNow())}
+                </p>
+              </div>
+            </div>
+          </Link>
+        ))
+      }
+
+    </div>
+  );
 
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -118,12 +118,12 @@ const Navbar = ({ settoggle }) => {
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                             </svg></div>
-                        <div className='removemic bg-zinc-600 text-zinc-300 w-8 rounded-full pt-2 pl-2 ml-2 h-8 cursor-pointer' onClick={startVoiceSearch}>
+                        <button className='removemic bg-zinc-600 text-zinc-300 w-8 rounded-full pl-2 ml-2 h-8 cursor-pointer' onClick={startVoiceSearch}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16">
                                 <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0z" />
                                 <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5" />
                             </svg>
-                        </div>
+                        </button>
                     </div>
 
                     <div className='flex text-zinc-300 items-center justify-evenly '>
